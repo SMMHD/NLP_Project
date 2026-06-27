1ace84f7f3ded0b4665d7f1dd12b2756f9b0c0f1
Moein <moeinhosseini2003@gmail.com>
2026-06-27 06:14:02 +0330

    Add the config.py code

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+DATA_DIR = "data"
+DB_DIR = "faiss_db"
+
+# تغییر مدل به یک مدل سبک‌تر و متفاوت
+EMBEDDING_MODEL = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+
+GROQ_API_KEY = "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" # کلید API برادرت یا همان کلید قبلی را بگذار
+
